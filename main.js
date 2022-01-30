bcaa47e6f1125218576b5183eace32a557216cc1
Arsenyuk Danil <90194057+sl14888@users.noreply.github.com>
2022-01-30 19:23:08 +0300

    prev value currencies

--- main.js
+++ main.js
@@ -5,7 +5,10 @@
 //   .then((data) => {
 //     console.log(data);
 //   });
+
+// заносим в глобальный массив
 const rates = {};
+// поиск карточек
 const elemUSD = document.querySelector('[data-value="USD"]');
 const elemEUR = document.querySelector('[data-value="EUR"]');
 const elemGBP = document.querySelector('[data-value="GBP"]');
@@ -13,6 +16,7 @@ const elemGBP = document.querySelector('[data-value="GBP"]');
 getCurrencies();
 
 async function getCurrencies() {
+  // получаем данные с ЦБ РФ
   const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
   const data = await response.json();
   const result = await data;
@@ -20,33 +24,41 @@ async function getCurrencies() {
   rates.EUR = result.Valute.EUR;
   rates.GBP = result.Valute.GBP;
 
-  console.log(rates);
-
+  // округляю значения в таблице
   elemUSD.textContent = rates.USD.Value.toFixed(2);
   elemEUR.textContent = rates.EUR.Value.toFixed(2);
   elemGBP.textContent = rates.GBP.Value.toFixed(2);
+  // вывод предыдущего значения в таблицу
+  elemEUR.nextElementSibling.textContent += ' ' + rates.EUR.Previous.toFixed(1);
+  elemUSD.nextElementSibling.textContent += ' ' + rates.USD.Previous.toFixed(1);
+  elemGBP.nextElementSibling.textContent += ' ' + rates.GBP.Previous.toFixed(1);
+
   // цвет для информера USD
   if (rates.USD.Value > rates.USD.Previous) {
     elemUSD.classList.add('top');
-    elemUSD.classList.remove('bottom');
+    elemUSD.textContent += ' ▲';
   } else {
+    elemUSD.textContent += ' ▼';
     elemUSD.classList.add('bottom');
-    elemUSD.classList.remove('top');
   }
   // цвет для информера EUR
   if (rates.EUR.Value > rates.EUR.Previous) {
     elemEUR.classList.add('top');
+    elemEUR.textContent += ' ▲';
     elemEUR.classList.remove('bottom');
   } else {
     elemEUR.classList.add('bottom');
+    elemEUR.textContent += ' ▼';
     elemEUR.classList.remove('top');
   }
   // цвет для информера GBP
   if (rates.GBP.Value > rates.GBP.Previous) {
     elemGBP.classList.add('top');
+    elemGBP.textContent += ' ▲';
     elemGBP.classList.remove('bottom');
   } else {
     elemGBP.classList.add('bottom');
+    elemGBP.textContent += ' ▼';
     elemGBP.classList.remove('top');
   }
 }
