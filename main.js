2172576490342e28d82ca7d7ec96c5fddc805cb3
Arsenyuk Danil <90194057+sl14888@users.noreply.github.com>
2022-01-30 18:20:40 +0300

    color informer currencies

--- main.js
+++ main.js
@@ -6,11 +6,12 @@
 //     console.log(data);
 //   });
 const rates = {};
-const elememUSD = document.querySelector('[data-value="USD"]');
-const elememEUR = document.querySelector('[data-value="EUR"]');
-const elememGBP = document.querySelector('[data-value="GBP"]');
+const elemUSD = document.querySelector('[data-value="USD"]');
+const elemEUR = document.querySelector('[data-value="EUR"]');
+const elemGBP = document.querySelector('[data-value="GBP"]');
 
 getCurrencies();
+
 async function getCurrencies() {
   const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
   const data = await response.json();
@@ -21,7 +22,31 @@ async function getCurrencies() {
 
   console.log(rates);
 
-  elememUSD.textContent = rates.USD.Value.toFixed(3);
-  elememEUR.textContent = rates.EUR.Value.toFixed(3);
-  elememGBP.textContent = rates.GBP.Value.toFixed(3);
+  elemUSD.textContent = rates.USD.Value.toFixed(2);
+  elemEUR.textContent = rates.EUR.Value.toFixed(2);
+  elemGBP.textContent = rates.GBP.Value.toFixed(2);
+  // цвет для информера USD
+  if (rates.USD.Value > rates.USD.Previous) {
+    elemUSD.classList.add('top');
+    elemUSD.classList.remove('bottom');
+  } else {
+    elemUSD.classList.add('bottom');
+    elemUSD.classList.remove('top');
+  }
+  // цвет для информера EUR
+  if (rates.EUR.Value > rates.EUR.Previous) {
+    elemEUR.classList.add('top');
+    elemEUR.classList.remove('bottom');
+  } else {
+    elemEUR.classList.add('bottom');
+    elemEUR.classList.remove('top');
+  }
+  // цвет для информера GBP
+  if (rates.GBP.Value > rates.GBP.Previous) {
+    elemGBP.classList.add('top');
+    elemGBP.classList.remove('bottom');
+  } else {
+    elemGBP.classList.add('bottom');
+    elemGBP.classList.remove('top');
+  }
 }
