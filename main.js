d900aba2799b9011c1c885f8f06c31218d77681a
Arsenyuk Danil <90194057+sl14888@users.noreply.github.com>
2022-01-30 19:59:37 +0300

    add convertor value

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-// fetch('https://www.cbr-xml-daily.ru/daily_json.js')
-//   .then((result) => {
-//     return result.json();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//   });
+fetch('https://www.cbr-xml-daily.ru/daily_json.js')
+  .then((result) => {
+    return result.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
 
 // заносим в глобальный массив
 const rates = {};
@@ -13,7 +13,14 @@ const elemUSD = document.querySelector('[data-value="USD"]');
 const elemEUR = document.querySelector('[data-value="EUR"]');
 const elemGBP = document.querySelector('[data-value="GBP"]');
 
+// поиск инпутов и селекторов
+const input = document.querySelector('#input');
+const result = document.querySelector('#result');
+const select = document.querySelector('#select');
+
 getCurrencies();
+
+setInterval(getCurrencies, 10000);
 
 async function getCurrencies() {
   // получаем данные с ЦБ РФ
@@ -28,10 +35,11 @@ async function getCurrencies() {
   elemUSD.textContent = rates.USD.Value.toFixed(2);
   elemEUR.textContent = rates.EUR.Value.toFixed(2);
   elemGBP.textContent = rates.GBP.Value.toFixed(2);
+
   // вывод предыдущего значения в таблицу
-  elemEUR.nextElementSibling.textContent += ' ' + rates.EUR.Previous.toFixed(1);
-  elemUSD.nextElementSibling.textContent += ' ' + rates.USD.Previous.toFixed(1);
-  elemGBP.nextElementSibling.textContent += ' ' + rates.GBP.Previous.toFixed(1);
+  elemEUR.nextElementSibling.textContent = 'предыдущее: ' + rates.EUR.Previous.toFixed(1);
+  elemUSD.nextElementSibling.textContent = 'предыдущее: ' + rates.USD.Previous.toFixed(1);
+  elemGBP.nextElementSibling.textContent = 'предыдущее: ' + rates.GBP.Previous.toFixed(1);
 
   // цвет для информера USD
   if (rates.USD.Value > rates.USD.Previous) {
@@ -61,4 +69,11 @@ async function getCurrencies() {
     elemGBP.textContent += ' ▼';
     elemGBP.classList.remove('top');
   }
+}
+
+input.oninput = convertValue;
+select.oninput = convertValue;
+
+function convertValue() {
+  result.value = (parseFloat(input.value) / rates[select.value].Value).toFixed(2);
 }
