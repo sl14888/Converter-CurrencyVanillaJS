7eee4958c8e27471f4b79da5ea0780851927d92e
Arsenyuk Danil <90194057+sl14888@users.noreply.github.com>
2022-01-30 18:11:49 +0300

    displaying the real exchange rate in a table

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log('Hello world!');
+// fetch('https://www.cbr-xml-daily.ru/daily_json.js')
+//   .then((result) => {
+//     return result.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
+const rates = {};
+const elememUSD = document.querySelector('[data-value="USD"]');
+const elememEUR = document.querySelector('[data-value="EUR"]');
+const elememGBP = document.querySelector('[data-value="GBP"]');
+
+getCurrencies();
+async function getCurrencies() {
+  const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
+  const data = await response.json();
+  const result = await data;
+  rates.USD = result.Valute.USD;
+  rates.EUR = result.Valute.EUR;
+  rates.GBP = result.Valute.GBP;
+
+  console.log(rates);
+
+  elememUSD.textContent = rates.USD.Value.toFixed(3);
+  elememEUR.textContent = rates.EUR.Value.toFixed(3);
+  elememGBP.textContent = rates.GBP.Value.toFixed(3);
+}
